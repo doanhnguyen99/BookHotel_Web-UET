13e5b1826cdfb9c742105d6dcbb044b12e17f601
Truong-AQ <nguyenvantruonglopso9@gmail.com>
2019-11-23 13:24:51 +0700

    sorry

--- BTLHotel/resources/views/master.blade.php
+++ BTLHotel/resources/views/master.blade.php
@@ -4,19 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="/library/carousel/owl.carousel.css">
-    <link rel="stylesheet" href="/library/carousel/owl.theme.default.css">
-    <link rel="stylesheet" href="/library/carousel/animate.css">
-    <link rel="stylesheet" href="/css/menu.css">
-    <link rel="stylesheet" href="/library/boostrap/bootstrap.min.css">
-    <link rel="stylesheet" href="/library/boostrap/bootstrap.css">
-    <link rel="stylesheet" href="/library/boostrap/font-awesome.css">
+    <base href="{{ asset('') }}">
+    <link rel="stylesheet" href="library/carousel/owl.carousel.css">
+    <link rel="stylesheet" href="library/carousel/owl.theme.default.css">
+    <link rel="stylesheet" href="library/carousel/animate.css">
+    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="library/boostrap/bootstrap.min.css">
+    <link rel="stylesheet" href="library/boostrap/bootstrap.css">
+    <link rel="stylesheet" href="library/boostrap/font-awesome.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/css/footer.css">
-    <link rel="stylesheet" href="/css/login.css">
-    <link rel="stylesheet" href="/css/index.css">
-    <link rel="stylesheet" href="/css/blog.css">
-    <link rel="stylesheet" href="/css/room.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/blog.css">
+    <link rel="stylesheet" href="css/room.css">
 
     <style type="text/css">
         h2.about{
@@ -62,13 +63,13 @@
     @yield('content')
     </div> <!-- .container -->
      @include('footer')
-    <script src="/library/jquery-3.4.1.js"></script>
-    <script src="/library/carousel/owl.carousel.js"></script>
-    <script src="/js/menu.js"></script>
-    <script src="/js/login.js"></script>
-    <script src="/library/boostrap/jquery-3.3.1.slim.min.js"></script>
-    <script src="/library/boostrap/popper.min.js"></script>
+    <script src="library/jquery-3.4.1.js"></script>
+    <script src="library/carousel/owl.carousel.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/login.js"></script>
+    <script src="library/boostrap/jquery-3.3.1.slim.min.js"></script>
+    <script src="library/boostrap/popper.min.js"></script>
     {{-- <script src="library/boostrap/bootstrap.min.js"></script> --}}
-    <script src="/library/boostrap/bootstrap.js"></script>
+    <script src="library/boostrap/bootstrap.js"></script>
 </body>
 </html>
